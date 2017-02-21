d599ad60796501ed17243c218303acd02d2a0aee
chuchodavidgomez <jesus.gomezo@udea.edu.co>
2017-02-21 23:15:43 +0100

    validaciones listas

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/ContactInfo.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/ContactInfo.java
@@ -3,11 +3,15 @@ package co.edu.udea.compumovil.gr08_20171.lab1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ContactInfo extends AppCompatActivity {
 
@@ -44,14 +48,16 @@ public class ContactInfo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Bundle bundle = getIntent().getExtras();
-                Intent sig2 = new Intent(ContactInfo.this, OtherInfo.class);
-                sig2.putExtra("Info2",bundle.getString("Info").toString()
-                        +"\n"+getString(R.string.numberContact)+":"+ etTelefono.getText().toString()
-                        +"\n"+getString(R.string.email)+":"+ etCorreo_Elect.getText().toString()
-                        +"\n"+getString(R.string.country)+":"+ etPais.getText().toString()
-                        +"\n"+getString(R.string.cityName)+":"+etCiudad.getText().toString()
-                        +"\n"+getString(R.string.direction)+":"+etDireccion.getText().toString());
-                startActivity(sig2);
+                if(validar()){
+                    Intent sig2 = new Intent(ContactInfo.this, OtherInfo.class);
+                    sig2.putExtra("Info2", bundle.getString("Info").toString()
+                            + "\n" + getString(R.string.numberContact) + ":" + etTelefono.getText().toString()
+                            + "\n" + getString(R.string.email) + ":" + etCorreo_Elect.getText().toString()
+                            + "\n" + getString(R.string.country) + ":" + etPais.getText().toString()
+                            + "\n" + getString(R.string.cityName) + ":" + etCiudad.getText().toString()
+                            + "\n" + getString(R.string.direction) + ":" + etDireccion.getText().toString());
+                    startActivity(sig2);
+                }
             }
         });
 
@@ -62,5 +68,107 @@ public class ContactInfo extends AppCompatActivity {
         AutoCompleteTextView ciudadesSugerencias = (AutoCompleteTextView) findViewById(R.id.campo_ciudad);
         ArrayAdapter<String> adaptador2 = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line,ciudad_prin);
         ciudadesSugerencias.setAdapter(adaptador2);
+    }
+
+    public boolean validar() {
+        boolean validado = true;
+        etTelefono.setError(null);
+        etCorreo_Elect.setError(null);
+        etPais.setError(null);
+        etCiudad.setError(null);
+        etDireccion.setError(null);
+
+        String telefono = etTelefono.getText().toString();
+        String correo = etCorreo_Elect.getText().toString();
+        String pais = etPais.getText().toString();
+        String ciudad = etCiudad.getText().toString();
+        String direccion = etDireccion.getText().toString();
+
+        if(TextUtils.isEmpty(telefono)){
+            etTelefono.setError(getString(R.string.error_campo_obligatorio));
+            etTelefono.requestFocus();
+            validado = false;
+        }
+
+        if(!isNumeric(telefono)){
+            etTelefono.setError(getString(R.string.error_campo_numerico));
+            etTelefono.requestFocus();
+            validado = false;
+        }
+
+        if(TextUtils.isEmpty(correo)){
+            etCorreo_Elect.setError(getString(R.string.error_campo_obligatorio));
+            etCorreo_Elect.requestFocus();
+            validado = false;
+        }
+
+        if(!validateEmail(correo)){
+            etCorreo_Elect.setError(getString(R.string.error_sinta));
+            etCorreo_Elect.requestFocus();
+            validado = false;
+        }
+
+        if(TextUtils.isEmpty(pais)){
+            etPais.setError(getString(R.string.error_campo_obligatorio));
+            etPais.requestFocus();
+            validado = false;
+        }
+
+        if(!checkAlpha(pais)){
+            etPais.setError(getString(R.string.error_campo_alfabetico));
+            etPais.requestFocus();
+            validado = false;
+        }
+
+        if(TextUtils.isEmpty(ciudad)){
+            etCiudad.setError(getString(R.string.error_campo_obligatorio));
+            etCiudad.requestFocus();
+            validado = false;
+        }
+
+        if(!checkAlpha(ciudad)){
+            etCiudad.setError(getString(R.string.error_campo_alfabetico));
+            etCiudad.requestFocus();
+            validado = false;
+        }
+
+        if(TextUtils.isEmpty(direccion)){
+            etDireccion.setError(getString(R.string.error_campo_obligatorio));
+            etDireccion.requestFocus();
+            validado = false;
+        }
+
+        return validado;
+    }
+
+    private static boolean isNumeric(String cadena){
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+
+    public static boolean checkAlpha(String str) {
+        boolean respuesta = false;
+        if ((str).matches("([a-z]|[A-Z]|\\s)+")) {
+            respuesta = true;
+        }
+        return respuesta;
+    }
+
+    private static final String PATTERN_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public static boolean validateEmail(String email) {
+
+        // Compiles the given regular expression into a pattern.
+        Pattern pattern = Pattern.compile(PATTERN_EMAIL);
+
+        // Match the given input against this pattern
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
     }
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/PersonalInfo.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/PersonalInfo.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -24,6 +26,7 @@ public class PersonalInfo extends AppCompatActivity implements DatePickerDialog.
     TextView etNacimiento;
     RadioButton rbSexo,rbSexoM,rbSexoF;
     Spinner spEduca;
+    boolean entro = false;
     private int dia, mes, año;
     private static DatePickerDialog.OnDateSetListener selectorFecha;
     @Override
@@ -42,12 +45,14 @@ public class PersonalInfo extends AppCompatActivity implements DatePickerDialog.
             @Override
             public void onClick(View v) {
                 rbSexo= (RadioButton)findViewById(R.id.radioButtonFemale);
+                entro = true;
             }
         });
         rbSexoM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rbSexo= (RadioButton)findViewById(R.id.radioButtonMale);
+                entro = true;
             }
         });
         btnMostrarFecha =(Button)findViewById(R.id.btnFechaNacimiento);
@@ -61,18 +66,65 @@ public class PersonalInfo extends AppCompatActivity implements DatePickerDialog.
         btnSiguiente1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(PersonalInfo.this,ContactInfo.class);
-
-                intent.putExtra("Info",getString(R.string.name)+": "+etNom.getText().toString()
-                        +"\n" + getString(R.string.lastName)+": "+etApe.getText().toString()
-                        +"\n" + getString(R.string.date)+": "+etNacimiento.getText().toString()
-                        +"\n" + getString(R.string.sex)+": "+rbSexo.getText().toString()
-                        +"\n" + getString(R.string.Educacion)+": "+spEduca.getSelectedItem().toString());
-                startActivity(intent);
+                if(validar()) {
+                    Intent intent = new Intent(PersonalInfo.this, ContactInfo.class);
+                    intent.putExtra("Info", getString(R.string.name) + ": " + etNom.getText().toString()
+                            + "\n" + getString(R.string.lastName) + ": " + etApe.getText().toString()
+                            + "\n" + getString(R.string.date) + ": " + etNacimiento.getText().toString()
+                            + "\n" + getString(R.string.sex) + ": " + rbSexo.getText().toString()
+                            + "\n" + getString(R.string.Educacion) + ": " + spEduca.getSelectedItem().toString());
+                    startActivity(intent);
+                }
             }
         });
         arraySpinner();
+    }
+
+    public boolean validar(){
+        boolean validado = true;
+        etNom.setError(null);
+        etApe.setError(null);
+
+        String nombre = etNom.getText().toString();
+        String Apellido = etApe.getText().toString();
+
+        if(TextUtils.isEmpty(nombre)){
+            etNom.setError(getString(R.string.error_campo_obligatorio));
+            etNom.requestFocus();
+            validado = false;
+        }
+
+        if(!checkAlpha(nombre)){
+            etNom.setError(getString(R.string.error_campo_alfabetico));
+            etNom.requestFocus();
+            validado = false;
+        }
+
+        if(TextUtils.isEmpty(Apellido)){
+            etApe.setError(getString(R.string.error_campo_obligatorio));
+            etApe.requestFocus();
+            validado = false;
+        }
+
+        if(!checkAlpha(Apellido)){
+            etApe.setError(getString(R.string.error_campo_alfabetico));
+            etApe.requestFocus();
+            validado = false;
+        }
+
+        if(!entro){
+            Toast.makeText(getApplicationContext(),"Debe seleccionar un sexo",Toast.LENGTH_LONG).show();
+            validado = false;
+        }
+        return validado;
+    }
+
+    public static boolean checkAlpha(String str) {
+        boolean respuesta = false;
+        if ((str).matches("([a-z]|[A-Z]|\\s)+")) {
+            respuesta = true;
+        }
+        return respuesta;
     }
 
     public void showDatePickerDialog(View v) {
