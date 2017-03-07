b2b88a7008ba8e27ae02107efd8cd66949c10496
nilto <nilton.velez@udea.edu.co>
2017-03-06 21:59:17 -0500

    splash ok

---
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr08_20171/lab2/Inicio.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr08_20171.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
